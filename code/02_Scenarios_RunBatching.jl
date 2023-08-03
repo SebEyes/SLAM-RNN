a9d04bc50ea3cde3b97b8cfef18f4973ef56dd6d
sebeyes <seb.lhoumeau@gmail.com>
2023-08-03 14:30:26 +0000

    Batch scenario 0 and test

---
+++ code/02_Scenarios_RunBatching.jl
@@ -0,0 +1,4 @@
+### Scenario 0: Best model selection
+include("S0[best_model_selection].jl")
+
+scenario_S0(10)
---
+++ code/S0[best_model_selection].jl
@@ -0,0 +1,48 @@
+### Load model
+include("01_Julia-RNN_model.jl")
+
+####
+## This script allow to save the model with the best accuracy after 20 trainings of the V6
+## Require between 12 to 24 hours to run
+####
+
+### Import data
+diversity_data = CSV.File(
+    "data/Matrix_dominant.csv",
+    delim = ";"
+) |> DataFrame
+
+function scenario_S0(number_runs::Int64)
+    accuracy_model = [0.0]
+
+    for run in 1:number_runs
+        @info("Run number $run/$number_runs")
+        model, output_model, all_accuracy, mean_accuracy, loss_model = forecast_model(
+            diversity_data, 
+            10_000, #between 30-60 of computation
+            "V6_best",
+            0, #No prediction
+            true #using sorted data
+            )
+        @info ("Mean Accuracy = $mean_accuracy")
+        
+
+        if mean_accuracy > last(accuracy_model) #if the new model has a better accuracy than the last one
+        ## Saving model
+            model_state = Flux.state(model)
+            jldsave("data/results_scenario/S0[best_model_selection]/V6_best.jld2"; 
+            model_state)
+            @info ("Saving Model")
+        end
+
+        append!(accuracy_model, mean_accuracy)
+    end
+
+    model_selection = DataFrame(
+        run = 0:number_runs,
+        mean_accuracy = accuracy_model
+    )
+
+    CSV.write("data/results_scenario/S0[best_model_selection]/modelV6 selection.csv", model_selection)
+    
+end
