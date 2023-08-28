14b9328ed78d0b856086182f9f6bc000d24c8914
sebeyes <seb.lhoumeau@gmail.com>
2023-08-28 10:03:20 +0000

    Plot Accurracy V6

--- code/04_Visualisation_res_scenario.R
+++ code/04_Visualisation_res_scenario.R
@@ -3,6 +3,7 @@ require(stringr)
 require(tidyr)
 require(dplyr)
 require(ggplot2)
+require(ggrepel)
 
 #####
 ## Mean accuracy model V6 with 50_000 epochs training (output S0)
@@ -15,18 +16,43 @@ acc_model = read.csv(
 # remove run 0
 acc_model = acc_model %>% filter(run > 0)
 
+acc_model$mean_accuracy = acc_model$mean_accuracy * 100
+
 ## Boxplot mean accuracy
-ggplot(
+BoxPlot_accurracy = ggplot(
     data = acc_model,
     aes(
+        x = "Accuracy model V6",
         y = mean_accuracy
     )
 ) + 
 geom_boxplot() +
-scale_y_continuous(limits = c(0,1)) + 
+scale_y_continuous(limits = c(0,100)) + 
+geom_text_repel(
+    data = subset(acc_model, acc_model$mean_accuracy == max(acc_model$mean_accuracy)),
+    aes(
+        label = paste(
+            "Best model (",
+            round(max(acc_model$mean_accuracy),1),
+            "%)",
+            sep = ""
+            )
+    ),
+    size = 5,
+    box.padding = unit(-7, "lines")
+)+ 
 labs(
     x = "",
-    y = "Mean accuracy"
+    y = "Mean accuracy (%)"
+) 
+
+BoxPlot_accurracy
+
+ggsave(
+    "docs/Accuracy_Model_V6.jpg",
+    width = 4,
+    height = 5,
+    units = "in"
 )
 
 ## Dispersion statistics
