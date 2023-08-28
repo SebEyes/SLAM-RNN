f82f04936aecdda62ab58861984cf24e52ae85f3
sebeyes <seb.lhoumeau@gmail.com>
2023-08-28 10:09:51 +0000

    Merge Results visualisation

--- code/03_Visualisation_res_scenario.R
+++ code/03_Visualisation_res_scenario.R
@@ -1,9 +1,67 @@
-### Loading Packages
+## Package
 require(stringr)
-require(dplyr)
 require(tidyr)
-require(reshape2)
+require(dplyr)
 require(ggplot2)
+require(ggrepel)
+require(reshape2)
+
+#####
+## Mean accuracy model V6 with 50_000 epochs training (output S0)
+#####
+
+## Database
+acc_model = read.csv(
+    "data/results_scenario/S0[best_model_selection]/modelV6 selection.csv"
+)
+# remove run 0
+acc_model = acc_model %>% filter(run > 0)
+
+acc_model$mean_accuracy = acc_model$mean_accuracy * 100
+
+## Boxplot mean accuracy
+BoxPlot_accurracy = ggplot(
+    data = acc_model,
+    aes(
+        x = "Accuracy model V6",
+        y = mean_accuracy
+    )
+) + 
+geom_boxplot() +
+scale_y_continuous(limits = c(0,100)) + 
+geom_text_repel(
+    data = subset(acc_model, acc_model$mean_accuracy == max(acc_model$mean_accuracy)),
+    aes(
+        label = paste(
+            "Best model (",
+            round(max(acc_model$mean_accuracy),1),
+            "%)",
+            sep = ""
+            )
+    ),
+    size = 5,
+    box.padding = unit(-7, "lines")
+)+ 
+labs(
+    x = "",
+    y = "Mean accuracy (%)"
+) 
+
+BoxPlot_accurracy
+
+ggsave(
+    "docs/Accuracy_Model_V6.jpg",
+    width = 4,
+    height = 5,
+    units = "in"
+)
+
+## Dispersion statistics
+summary(acc_model$mean_accuracy)
+
+#####
+## Forecasts model V6 with 50_000 epochs training (output S0)
+#####
 
 ### Load data
 # Output model
@@ -51,7 +109,8 @@ data_plot$label = paste(
     data_plot$variable,
     " (",
     data_plot$rounded,
-    "%)"
+    "%)",
+    sep = ""
 )
 
 ggplot(
